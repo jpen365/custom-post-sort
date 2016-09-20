12fdbab16c9d81c66842147f00518769f311f35e
Jon Penland <jon.penland@gmail.com>
2016-09-19 19:18:40 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# custom-post-sort
-A plugin for adding a custom post order field to WordPress posts.
+# Custom Post Sort
+
+This plugin adds a custom field to WordPress posts. Users can specify the order in which posts should appear by adding a numeric value to the custom field. 
+
+There are several ways to use the resulting custom field.
+
+1. Use `orderby` to limit the loop to just those posts that have been assigned a custom sort value and hook into the theme with `pre_get_posts`.
+2. Use an object of the `WP_Query` in a custom page template to create a curated list of ordered posts. 
---
+++ custom-post-sort.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Plugin Name:   Custom Post Sort
+ * Plugin URI:    https://github.com/jpen365/custom-post-sort
+ * Description:   Add a custom post order field to WordPress posts and display posts on the blog page using this new field.
+ * Version:       0.1
+ * Author:        Jon Penland
+ * Author URI:    http://www.jonpenland.com
+ * Text Domain:   custom-post-sort
+ */
+
+
+/* Create custom meta data box to the post edit screen */
+
+function jpen_custom_post_sort( $post ){
+  add_meta_box( 
+    'custom_post_sort_box', 
+    'Position in List of Posts', 
+    'jpen_custom_post_order', 
+    'post' ,
+    'side'
+    );
+}
+add_action( 'add_meta_boxes', 'jpen_custom_post_sort' );
+
+
+/* Add a field to the metabox */
+
+function jpen_custom_post_order( $post ) {
+  wp_nonce_field( basename( __FILE__ ), 'jpen_custom_post_order_nonce' );
+  $current_pos = get_post_meta( $post->ID, '_custom_post_order', true); ?>
+  <p>Enter the position at which you would like the post to appear. For exampe, post "1" will appear first, post "2" second, and so forth.</p>
+  <p><input type="number" name="pos" value="<?php echo $current_pos; ?>" /></p>
+  <p>Posts that are not assigned a number will appear after numbered posts and will be sorted by date of publication.</p>
+  <?php
+}
+
+
+/* Save the input to post_meta_data */
+
+function jpen_save_custom_post_order( $post_id ){
+  if ( !isset( $_POST['jpen_custom_post_order_nonce'] ) || !wp_verify_nonce( $_POST['jpen_custom_post_order_nonce'], basename( __FILE__ ) ) ){
+    return;
+  } 
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+    return;
+  }
+  if ( ! current_user_can( 'edit_post', $post_id ) ){
+    return;
+  }
+  if ( isset( $_REQUEST['pos'] ) ) {
+    update_post_meta( $post_id, '_custom_post_order', sanitize_text_field( $_POST['pos'] ) );
+  }
+}
+add_action( 'save_post', 'jpen_save_custom_post_order' );
+
+
+/* Add custom post order column to post list */
+
+function jpen_add_custom_post_order_column( $columns ){
+  return array_merge ( $columns,
+    array( 'pos' => 'Position', ));
+}
+add_filter('manage_posts_columns' , 'jpen_add_custom_post_order_column');
+
+
+/* Display custom post order in the post list */
+
+function jpen_custom_post_order_value( $column, $post_id ){
+  if ($column == 'pos' ){
+    echo '<p>' . get_post_meta( $post_id, '_custom_post_order', true) . '</p>';
+  }
+}
+add_action( 'manage_posts_custom_column' , 'jpen_custom_post_order_value' , 10 , 2 );
+
+
+/* Use pre_get_posts to sort by post_meta_data */
+
+function jpen_custom_post_order_sort( $query ){
+  if ( is_home() ){
+    $query->set( 'orderby', 'meta_value' );
+    $query->set( 'meta_key', '_custom_post_order' );
+    $query->set( 'order' , 'ASC' );
+  }
+}
+add_action( 'pre_get_posts' , 'jpen_custom_post_order_sort' );
